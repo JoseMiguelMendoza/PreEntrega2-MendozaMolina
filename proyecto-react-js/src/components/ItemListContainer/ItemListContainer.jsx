ea589ed625eaf66ef8abc430ab68f0a33da4514c
Jose Mendoza <jose.miguel.mendoza.molina@gmail.com>
2023-03-11 17:39:23 -0300

    añadiendo prop greeting

--- proyecto-react-js/src/components/ItemListContainer/ItemListContainer.jsx
+++ proyecto-react-js/src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,10 +1,10 @@
 import styles from "./ItemListContainer.module.scss"
 
-const ItemListContainer = () => {
+const ItemListContainer = ({greeting}) => {
   return (
     <div className={styles.contenedorGeneral}>
         <div className={styles.contenedorTexto}>
-            <p className={styles.texto}>¡Bienvenidos a ForTrading!</p>
+            <p className={styles.texto}>{greeting}</p>
             <p className={styles.texto}>¡Espero disfruten de una linda experiencia navegando por nuestra página!</p>
         </div>
     </div>
